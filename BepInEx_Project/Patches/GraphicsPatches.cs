523fc857fd99dfef193451b543ae34ad6489af4f
KingKrouch <64479044+KingKrouch@users.noreply.github.com>
2024-02-06 14:54:56 -0500

    Add a GPU utilization fix for toggling off SSAO when outside of gameplay and cutscenes.

--- BepInEx_Project/Patches/GraphicsPatches.cs
+++ BepInEx_Project/Patches/GraphicsPatches.cs
@@ -4,6 +4,8 @@ using HarmonyLib;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using System.Reflection;
+using Game.UI.Title;
+using UnityEngine.Experimental.Rendering.Universal;
 
 namespace SvSFix;
 
@@ -12,53 +14,37 @@ public partial class SvSFix
     [HarmonyPatch]
     public class GraphicsPatches
     {
-        // TODO: Figure out why this won't work.
+        // TODO: Further test this to see if this fixes the framerate issues on the Steam Deck.
         public static void ToggleSSAO(bool toggle)
         {
-            // Let's adjust some of the Render Pipeline Settings during runtime.
-            var asset = (UniversalRenderPipelineAsset)QualitySettings.renderPipeline;
-            if (asset != null)
-            {
-                // Use reflection to access internal field 'm_RendererDataList'
-                FieldInfo rendererDataListField = typeof(UniversalRenderPipelineAsset).GetField("m_RendererDataList", BindingFlags.NonPublic | BindingFlags.Instance);
-
-                if (rendererDataListField != null)
-                {
-                    // Get the current UniversalRendererData
-                    UniversalRendererData[] rendererDataList = (UniversalRendererData[])rendererDataListField.GetValue(asset);
-
-                    if (rendererDataList != null && rendererDataList.Length > 0)
-                    {
-                        // Iterate through renderer data list
-                        foreach (UniversalRendererData rendererData in rendererDataList)
-                        {
-                            // Get the current rendering features
-                            List<ScriptableRendererFeature> renderingFeatures = rendererData.rendererFeatures;
-
-                            // Find the SSAO feature using reflection and toggle it off
-                            foreach (ScriptableRendererFeature feature in renderingFeatures)
-                            {
-                                if (feature.GetType().Name == "ScreenSpaceAmbientOcclusion")
-                                {
-                                    FieldInfo enabledField = feature.GetType().GetField("m_Enabled", BindingFlags.NonPublic | BindingFlags.Instance);
-                                    if (enabledField != null)
-                                    {
-                                        enabledField.SetValue(feature, toggle);
-                                        switch (toggle)
-                                        {
-                                            case true:
-                                                Debug.Log("SSAO has been toggled on!");
-                                                break;
-                                            case false:
-                                                Debug.Log("SSAO has been toggled off!");
-                                                break;
-                                        }
-                                        return; // Exit once we found and toggled it
-                                    }
+            var renderPipeline = QualitySettings.renderPipeline;
+            Debug.Log("Render pipeline type: " + renderPipeline.GetType().ToString());
+            switch (renderPipeline is UniversalRenderPipelineAsset) {
+                case true: {
+                    var asset = QualitySettings.renderPipeline as UniversalRenderPipelineAsset;
+                    // Use reflection to access the 'rendererFeatures' property
+                    FieldInfo rendererDataList        = asset.GetType().GetField("m_RendererDataList", BindingFlags.Instance | BindingFlags.NonPublic);
+                    if (rendererDataList == null) {
+                        _log.LogError("RendererDataList returned Null.");
+                        return;
+                    }
+                    var scriptableRendererData = ((ScriptableRendererData[])rendererDataList?.GetValue(asset));
+                    if (scriptableRendererData != null && scriptableRendererData.Length > 0) {
+                        foreach (var rendererData in scriptableRendererData) {
+                            foreach (var rendererFeature in rendererData.rendererFeatures) {
+                                if (rendererFeature.name == "SSAO") {
+                                    _log.LogInfo("SSAO Found! " + (toggle ? "Toggling On." : "Toggling Off."));
+                                    rendererFeature.SetActive(toggle);
+                                    return;
                                 }
                             }
                         }
                     }
+                    break;
+                }
+                case false: {
+                    Debug.LogError("Render pipeline is not of type UniversalRenderPipelineAsset.");
+                    break;
                 }
             }
         }
@@ -73,6 +59,7 @@ public partial class SvSFix
         }
         
         [HarmonyPatch(typeof(MapPlay), nameof(MapPlay.MapFree))]
+        [HarmonyPatch(typeof(GameUiTitle), nameof(GameUiTitle.Ready))]
         [HarmonyPrefix]
         public static void MapReleaseHook()
         {
